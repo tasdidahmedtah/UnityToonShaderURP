26b32de3c0da9ad4d6d2ceb07218e377d63e450a
Aiden_Code <104345733+tasdidahmedtah@users.noreply.github.com>
2022-10-05 08:04:46 -0600

    Add files via upload

---
+++ Assets/Shaders/ToonShader/CaneraRotate/CameraRotaion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotaion : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody rb;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(0, Speed * Time.deltaTime, 0);
+    }
+}
